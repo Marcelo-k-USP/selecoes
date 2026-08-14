4c4916c4005d141a5859ce4aa3b95ea892320cdc
Marcelo-k-USP <makf00@gmail.com>
2026-08-14 14:59:12 -0300

    correção de bugs em mais seeders

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Categoria;
+use App\Models\Vinculo;
 use Illuminate\Database\Seeder;
 
 class CategoriaSeeder extends Seeder
@@ -14,14 +15,28 @@ class CategoriaSeeder extends Seeder
      */
     public function run()
     {
+        $vinculo_posgraduacao_id = Vinculo::where('nome', 'Pós-Graduação')->value('id');
+
         $categorias = [
             [
+                'vinculo_id' => $vinculo_posgraduacao_id,
                 'nome' => 'Aluno Regular',
-                'descricao' => 'Mestrado e Doutorado'
+                'descricao' => 'Mestrado e Doutorado',
+                'exige_programa' => true,
+                'exige_nivel' => true,
+                'exige_linhapesquisa' => true,
+                'exige_disciplinas' => false,
+                'processos' => 'Inscrição e Matrícula',
             ],
             [
+                'vinculo_id' => $vinculo_posgraduacao_id,
                 'nome' => 'Aluno Especial',
-                'descricao' => 'Em disciplinas'
+                'descricao' => 'Em disciplinas',
+                'exige_programa' => false,
+                'exige_nivel' => false,
+                'exige_linhapesquisa' => false,
+                'exige_disciplinas' => true,
+                'processos' => 'Matrícula',
             ],
         ];
 
